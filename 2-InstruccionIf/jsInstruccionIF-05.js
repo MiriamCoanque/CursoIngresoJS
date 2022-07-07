d0e08a01ab57b8acf08a00cb08721246339fa507
Miriam Coanque <NekoNyaaann@gmail.com>
2022-07-07 17:30:35 -0300

    nuevo cambio

--- 2-InstruccionIf/jsInstruccionIF-05.js
+++ 2-InstruccionIf/jsInstruccionIF-05.js
@@ -1,6 +1,15 @@
+/*Al ingresar una edad solo debemos informar si la persona NO es adolescente.*/
+
 function mostrar()
 {
-	//tomo la edad  
-	alert("ok");
+	let edad;
+
+	edad=document.getElementById("txtIdEdad").value;
+	edad=parseInt(edad);
+
+	if(edad<13 || edad>17)
+	{
+		alert("no eres adolescente");
+	}
 
 }//FIN DE LA FUNCIÓN
--- 2-InstruccionIf/jsInstruccionIF-06.js
+++ 2-InstruccionIf/jsInstruccionIF-06.js
@@ -1,7 +1,26 @@
+/*Al ingresar una edad debemos informar
+si la persona es mayor de edad (mas de 18 años) o adolescente (entre 13 y 17 años) o niño (menor a 13 años).*/
+
 function mostrar()
 {
-	//tomo la edad  
-	alert("ok");
+	let edad;
+
+	edad=document.getElementById("txtIdEdad").value;
+	edad=parseInt(edad);
+
+	if(edad<13)
+	{
+		alert("eres un niño");
+	}
+	else
+	{
+		if(edad<18)
+			alert("eres adolescente");
+		else
+		{
+			alert("eres mayor de edad");
+		}
+	}
 
 
 
